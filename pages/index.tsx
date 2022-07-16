35c538cf49fe7f8cc71336c952426f80a39d0cf3
Rossi Kamal <rossikamal@gmail.com>
2022-07-17 00:23:19 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,22 +13,22 @@ const IndexPage = () => {
         <div className="container">
           <article style={{backgroundImage: 'url(/images/featured-1.jpg)'}} className="featured-item featured-item-large">
             <div className="featured-item__content">
-              <h3>New arrivals are now in!</h3>
-              <a href="#" className="btn btn--rounded">Show Collection</a>
+              <h3>Seasonal and leafy vegetable at 10% Sale</h3>
+              <a href="#" className="btn btn--rounded">View All</a>
             </div>
           </article>
           
           <article style={{backgroundImage: 'url(/images/featured-2.jpg)'}} className="featured-item featured-item-small-first">
             <div className="featured-item__content">
-              <h3>Basic t-shirts $29,99</h3>
-              <a href="#" className="btn btn--rounded">More details</a>
+              <h3>Imported fruits on 20% Sales</h3>
+              <a href="#" className="btn btn--rounded">View All</a>
             </div>
           </article>
           
           <article style={{backgroundImage: 'url(/images/featured-3.jpg)'}} className="featured-item featured-item-small">
             <div className="featured-item__content">
-              <h3>Sale this summer</h3>
-              <a href="#" className="btn btn--rounded">VIEW ALL</a>
+              <h3>Local fruits on 10% Sale</h3>
+              <a href="#" className="btn btn--rounded">View All</a>
             </div>
           </article>
         </div>
@@ -37,40 +37,39 @@ const IndexPage = () => {
       <section className="section">
         <div className="container">
           <header className="section__intro">
-            <h4>Why should you choose us?</h4>
+            <h4>Our Specialities</h4>
           </header>
 
           <ul className="shop-data-items">
             <li>
               <i className="icon-shipping"></i>
               <div className="data-item__content">
-                <h4>Free Shipping</h4>
-                <p>All purchases over $199 are eligible for free shipping via USPS First Class Mail.</p>
+                <h4>Fresh Vegetable</h4>
+                <p>All vegetables are available at your doorstep within hour</p>
               </div>
             </li>
             
             <li>
               <i className="icon-payment"></i>
               <div className="data-item__content">
-                <h4>Easy Payments</h4>
-                <p>All payments are processed instantly over a secure payment protocol.</p>
+                <h4>Imported Fruit</h4>
+                <p>We bring your fruits from western countries with good quality</p>
               </div>
             </li>
             
             <li>
               <i className="icon-cash"></i>
               <div className="data-item__content">
-                <h4>Money-Back Guarantee</h4>
-                <p>If an item arrived damaged or you've changed your mind, you can send it
-                back for a full refund.</p>
+                <h4>No--Chemical</h4>
+                <p>In-house farming, no chemical is guranteed.</p>
               </div>
             </li>
             
             <li>
               <i className="icon-materials"></i>
               <div className="data-item__content">
-                <h4>Finest Quality</h4>
-                <p>Designed to last, each of our products has been crafted with the finest materials.</p>
+                <h4>Cheaper Price</h4>
+                <p>We brought you with the minimal and garden price.</p>
               </div>
             </li>
           </ul>
