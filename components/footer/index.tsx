ba4cddb291b17fe2fd63425dc90a4b9bba17b30e
Rossi Kamal <rossikamal@gmail.com>
2022-07-16 14:55:38 +0600

    Update index.tsx

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -7,38 +7,30 @@ const Footer = () => {
         <div className="site-footer__top">
           <div className="site-footer__description">
             <h6><Logo /> <span>E</span>-Shop</h6>
-            <p>House My Brand designs clothing for the young, the old & everyone in between – but most 
-              importantly, for the fashionable</p>
+            <p>GoodMart brings you fresh vegetable and fruit at the cheapest cost at your doorsteps</p>
             <ul className="site-footer__social-networks">
-              <li><a href="#"><i className="icon-facebook"></i></a></li>
-              <li><a href="#"><i className="icon-twitter"></i></a></li>
-              <li><a href="#"><i className="icon-linkedin"></i></a></li>
-              <li><a href="#"><i className="icon-instagram"></i></a></li>
               <li><a href="#"><i className="icon-youtube-play"></i></a></li>
             </ul>
           </div>
 
           <div className="site-footer__links">
             <ul>
-              <li>Shopping online</li>
-              <li><a href="#">Order Status</a></li>
-              <li><a href="#">Shipping and Delivery</a></li>
-              <li><a href="#">Returns</a></li>
-              <li><a href="#">Payment options</a></li>
-              <li><a href="#">Contact Us</a></li>
+              <li>Product</li>
+              <li><a href="#">Veg</a></li>
+              <li><a href="#">Fruit</a></li>
+              <li><a href="#">Other</a></li>
+              
             </ul>
             <ul>
-              <li>Information</li>
-              <li><a href="#">Gift Cards</a></li>
-              <li><a href="#">Find a store</a></li>
-              <li><a href="#">Newsletter</a></li>
-              <li><a href="#">Bacome a member</a></li>
-              <li><a href="#">Site feedback</a></li>
+              <li>Deal</li>
+              <li><a href="#">New Arrival</a></li>
+              <li><a href="#">Summer Festival</a></li>
+              
             </ul>
             <ul>
-              <li>Contact</li>
-              <li><a href="#">store@uikit.com</a></li>
-              <li><a href="#">Hotline: +1 131 138 138</a></li>
+              <li>About us</li>
+              <li><a href="#">info@greenmart.xyz</a></li>
+              <li><a href="#">Hotline: +42 33434 343234 33</a></li>
             </ul>
           </div>
         </div>
@@ -46,7 +38,7 @@ const Footer = () => {
       
       <div className="site-footer__bottom">
         <div className="container">
-          <p>DESIGN BY ICEO.CO - © 2019. ALL RIGHTS RESERVED.</p>
+          <p>GoodMart 2022- Your Green Partner.</p>
         </div>
       </div>
     </footer>
