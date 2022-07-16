846f994c8927080fad9e18fd132e49b96320aa46
Rossi Kamal <rossikamal@gmail.com>
2022-07-16 19:28:24 +0600

    Update index.tsx

--- components/subscribe/index.tsx
+++ components/subscribe/index.tsx
@@ -2,7 +2,7 @@ const Subscribe = () => {
   return (
     <section className="subscribe">
       <div className="container">
-        <div style={{backgroundImage: 'url(/images/a0.png)'}} className="subscribe__content">
+        <div style={{backgroundImage: 'url(/images/io.png)'}} className="subscribe__content">
           <h4>Enjoy 10% discount on summer fruit arrivals</h4>
 
           <form className="subscribe__form">
